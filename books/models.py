ef22d1b98857f4a9f284b397324232a8e0989081
zahra imen <zahraimen91@gmail.com>
2022-04-23 19:21:03 +0430

    add Book Detail View

--- books/models.py
+++ books/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 
+from django.urls import reverse
+
 
 # Create your models here.
 class Book(models.Model):
@@ -10,3 +12,6 @@ class Book(models.Model):
 
     def __str__(self):
         return self.title
+
+    def get_absolute_url(self):
+        return reverse('book_detail',args=[self.id])
--- books/urls.py
+++ books/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.BookListView.as_view(), name='book_list')
+    path('', views.BookListView.as_view(), name='book_list'),
+    path('<int:pk>/', views.BookDetailView.as_view(), name='book_detail'),
 ]
