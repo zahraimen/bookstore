f23c7ec01063ffbaad21ce71ed7bf6dd1309c0f5
zahra imen <zahraimen91@gmail.com>
2022-04-24 17:25:50 +0430

    add statics for book_list , book_detail , book_create

--- config/settings.py
+++ config/settings.py
@@ -117,8 +117,6 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = 'static/'
-
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
@@ -134,3 +132,7 @@ CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 # email config
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+# static file config
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [str(BASE_DIR.joinpath('static'))]
