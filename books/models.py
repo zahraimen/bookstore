4e699add51e1c1bb8278913a9b2d211ad7e6ca26
zahra imen <zahraimen91@gmail.com>
2022-04-26 12:52:00 +0430

    Add field cover to book

--- books/models.py
+++ books/models.py
@@ -9,9 +9,10 @@ class Book(models.Model):
     author = models.CharField(max_length=200)
     content = models.TextField()
     price = models.DecimalField(max_digits=5, decimal_places=2)
+    cover = models.ImageField(upload_to='covers/', blank=True)
 
     def __str__(self):
         return self.title
 
     def get_absolute_url(self):
-        return reverse('book_detail',args=[self.id])
+        return reverse('book_detail', args=[self.id])
