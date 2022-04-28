0dab8c71ccf0a9ffd21ee5cd897e0909c1a6c7ec
zahra imen <zahraimen91@gmail.com>
2022-04-28 14:28:13 +0430

    add commen model to admin register

--- books/admin.py
+++ books/admin.py
@@ -1,12 +1,12 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Book
+from .models import Book, Comment
 
 
-# @admin.register(Comment)
-# class CommentAdmin(admin.ModelAdmin):
-#     list_display = ('user', 'book', 'text', 'datetime_created')
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('user', 'book', 'text', 'datetime_created')
 
 
 admin.site.register(Book)
