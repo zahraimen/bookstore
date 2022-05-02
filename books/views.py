d73269507a85eb55c96aac6b65c4a97465a170b8
zahra imen <zahraimen91@gmail.com>
2022-05-03 02:38:18 +0430

    add permission for edite and delete objects

--- books/views.py
+++ books/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, redirect, reverse
 from django.views import generic
 from django.urls import reverse_lazy
 from django.shortcuts import get_object_or_404
-from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from django.contrib.auth.decorators import login_required
 # Create your views here.
 from .models import Book
@@ -50,13 +50,21 @@ class BookCreateView(LoginRequiredMixin, generic.CreateView):
     template_name = 'books/book_create.html'
 
 
-class BookUpdateView(LoginRequiredMixin, generic.UpdateView):
+class BookUpdateView(LoginRequiredMixin, UserPassesTestMixin, generic.UpdateView):
     model = Book
     fields = ['title', 'author', 'content', 'price', 'cover', ]
     template_name = 'books/book_update.html'
+
+    def test_func(self):
+        obj = self.get_object()
+        return obj.user == self.request.user
 
 
 class BookDeleteView(LoginRequiredMixin, generic.DeleteView):
     model = Book
     template_name = 'books/book_delete.html'
     success_url = reverse_lazy('book_list')
+
+    def test_func(self):
+        obj = self.get_object()
+        return obj.user == self.request.user
